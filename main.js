eb68e78c8190ce332f97ac33675d92d0dde82d1f
CarmeloMelito <melitocarmelo211@gmail.com>
2025-03-13 20:30:27 +0100

    feat:call api

---
+++ main.js
@@ -0,0 +1,16 @@
+let randoMail = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+axios.get(randoMail)
+    .then((response) => {
+        console.log(response);
+
+
+
+
+
+
+
+
+
+
+    })
