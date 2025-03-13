b265c829ffdc7373e77b090cc4a70ffa6c3c1231
CarmeloMelito <melitocarmelo211@gmail.com>
2025-03-13 21:21:36 +0100

    feat:create list item

--- main.js
+++ main.js
@@ -6,45 +6,27 @@ for (let i = 0; i < 10; i++) {
     axios.get(randoMail)
         .then(response => {
             emails.push(response.data.response)
-            console.log(emails)
+            // console.log(emails)
+            let emailList = document.getElementById("email-list");
+
+            let email = response.data.response;
+            emails.push(email);
+            //Creazione LI
+            let listItem = document.createElement("li");
+            listItem.textContent = email;
+            emailList.appendChild(listItem);
+            console.log(email);
         })
+        //ERRORE
+        .catch(error => {
+            console.log("ERRORE");
+        });
 }
 
 
-
-
-
-
-
-
-
-
-
-
-/*
-//AXIOS
-// axios.get(randoMail)
-//     .then((response) => {
-//         console.log(response.data);
-
-
-//         const mail = response.data;
-
-
-
-for (let i = 0; i > mail.lenght; i++) {
-
-    const mail = response.data;
-    axios.get(randoMail)
-        .then((response) => {
-            console.log(response.data);
-
-
-        })
-}
-*/
-
-
+// .catch(error => {
+//     console.log("ERRORE")
+// });
 
 
 //   <ul id="email-list">
